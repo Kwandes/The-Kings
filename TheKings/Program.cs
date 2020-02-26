215fa6b0dd103926fcfa96180e0c88bd202e956a
Jan <jan.bogoryja@gmail.com>
2020-02-27 00:05:25 +0100

    Change DeSerialization and Gist Getting, Tweak properties
    Change the way of handling the json data, switch from getStringAsync() to getStreamAsync()
    Change data type from List to IEnumerable<>
    Tweak King properties to have proper names (impossible previously due to a different deserialisation method), add new properties for years in reign

--- TheKings/Program.cs
+++ TheKings/Program.cs
@@ -1,69 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Newtonsoft.Json;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TheKings
 {
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        private static readonly HttpClient Client = new HttpClient();
 
-        private static async Task Main(string[] args)
+        private static async Task Main()
         {
-            #region Get the gist data from provided gistID
-
-            string gistData = "";
-
-            try
-            {
-                gistData = await GetGistDataAsync("10d65ccef9f29de3acd49d97ed423736");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                // There is nothing to do if the API request has failed, close the application
-                System.Environment.Exit(0);
-            }
-
-            #endregion
+            // Get the gist data from provided gistID
+            var gistData =
+                await GetGistDataAsync(
+                    "https://gist.githubusercontent.com/christianpanton/10d65ccef9f29de3acd49d97ed423736/raw/b09563bc0c4b318132c7a738e679d4f984ef0048");
 
             // Convert the gist data to a usable format
-            List<King> kings = null;
-            try
+
+            var kings = await JsonSerializer.DeserializeAsync<IEnumerable<King>>(gistData);
+
+            // Check for 
+            if (kings == null)
             {
-                kings = ConvertGistData(gistData) ?? throw new ArgumentNullException();
-            }
-            catch (ArgumentNullException)
-            {
-                Console.WriteLine("No usable data, closing the application");
-                System.Environment.Exit(0);
+                Console.WriteLine("No data, closing the application");
+                Environment.Exit(0);
             }
 
             // Process the kings data and answer questions
             AnswerQuestions(kings);
         }
 
-        private static void AnswerQuestions(List<King> kings)
+        private static void AnswerQuestions(IEnumerable<King> kings)
         {
             // Question 1
             Console.WriteLine("Q: How many monarchs are there in the list?");
-            Console.WriteLine($"A: There are {kings.Count} monarchs in the list\n");
+            Console.WriteLine($"A: There are {kings.Count()} monarchs in the list\n");
             // alternate answer using LinQ: kings.Last().id
 
             // Question 2
             Console.WriteLine("Q: Which monarch ruled the longest (and for how long)?");
 
             // Get longest ruling monarch
-            var longestRulingMonarch = kings.Aggregate((x1, x2) => x1.GetRuleTime() > x2.GetRuleTime() ? x1 : x2);
+            var longestRulingMonarch = kings.Aggregate((x1, x2) => x1.YearsInReign > x2.YearsInReign ? x1 : x2);
 
             Console.WriteLine(
-                $"A: The longest ruler was/is {longestRulingMonarch.nm} who ruled for {longestRulingMonarch.GetRuleTime()} years\n");
+                $"A: The longest ruler was/is {longestRulingMonarch.Name} who ruled for {longestRulingMonarch.YearsInReign} years\n");
 
             // Question 3
             Console.WriteLine("Q: Which house ruled the longest (and for how long)?");
@@ -73,15 +60,15 @@ namespace TheKings
             // Populate a dictionary with house data
             foreach (var king in kings)
             {
-                if (!houses.ContainsKey(king.hse))
+                if (!houses.ContainsKey(king.House))
                 {
-                    houses.Add(king.hse, king.GetRuleTime());
+                    houses.Add(king.House, king.YearsInReign);
                 }
                 else
                 {
                     int newValue;
-                    houses.TryGetValue(king.hse, out newValue);
-                    houses[king.hse] = newValue + king.GetRuleTime();
+                    houses.TryGetValue(king.House, out newValue);
+                    houses[king.House] = newValue + king.YearsInReign;
                 }
             }
 
@@ -98,7 +85,7 @@ namespace TheKings
             // Populate the dictionary with the monarch names
             foreach (var king in kings)
             {
-                var name = king.nm.Split(" ")[0];
+                var name = king.Name.Split(" ")[0];
                 if (!names.ContainsKey(name))
                 {
                     names.Add(name, 1);
@@ -118,116 +105,65 @@ namespace TheKings
         }
 
         // Get gist data using Github API
-        private static async Task<string> GetGistDataAsync(string gistId)
+        private static async Task<Stream> GetGistDataAsync(string gistLink)
         {
             // Specify the request header
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Gist Test");
+            Client.DefaultRequestHeaders.Add("User-Agent", ".NET Gist Test");
 
             // Fetch gist data
-            var stringTask = client.GetStringAsync("https://api.github.com/gists/" + gistId);
-
-            return await stringTask;
-        }
-
-        private static List<King> ConvertGistData(string gistData)
-        {
-            var kings = new List<King>();
-
-            // Separate actual file contents from other Json data
-            var contentsPattern = @",+\Wcontent\W:(\W\[\\n\s\s{.+}\\n\W)";
-            Regex re = new Regex(contentsPattern);
-            try
-            {
-                gistData = re.Match(gistData).Groups[1].ToString();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Failed to convert the data\n" + e);
-                return null;
-            }
-
-            // Remove extra characters
-            gistData = gistData.Replace(@"\n", "").Replace(@"\", "");
-
-            // Split into separate Json Object string for easy parsing
-            // The Json serializer can't handle all of the data at once
-            string[] splitGistData = Regex.Split(gistData, @"(?<=},)");
-
-            // Trim the split data (trailing ,)
-            for (int i = 0; i < splitGistData.Length; i++)
-            {
-                if (i == 0)
-                {
-                    splitGistData[i] = splitGistData[i].Remove(0, 4);
-                    splitGistData[i] = splitGistData[i].Remove(splitGistData[i].Length - 1, 1);
-                }
-                else
-                {
-                    splitGistData[i] = splitGistData[i].Remove(0, 2);
-                    splitGistData[i] = splitGistData[i].Remove(splitGistData[i].Length - 1, 1);
-                }
-            }
-
-            // Deserialize the data and add to the list
-            foreach (var data in splitGistData)
-            {
-                try
-                {
-                    kings.Add(JsonConvert.DeserializeObject<King>(data));
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Failed to deserialize {data}\n" + e);
-                }
-            }
-
-            return kings;
+            var streamTask = await Client.GetStreamAsync(gistLink);
+            return streamTask;
         }
     }
 
     // Object representation of the Json data
-    class King
+    public class King
     {
-        [JsonPropertyName("id")] public string id { get; set; }
+        private int _yearsInReign;
+        private string _datesRuled;
 
-        [JsonPropertyName("name")] public string nm { get; set; }
+        [JsonPropertyName("id")] public int Id { get; set; }
 
-        [JsonPropertyName("city")] public string cty { get; set; }
+        [JsonPropertyName("nm")] public string Name { get; set; }
 
-        [JsonPropertyName("house")] public string hse { get; set; }
+        [JsonPropertyName("cty")] public string City { get; set; }
 
-        [JsonPropertyName("years")] public string yrs { get; set; }
+        [JsonPropertyName("hse")] public string House { get; set; }
+
+        [JsonPropertyName("yrs")]
+        public string DatesRuled
+        {
+            get => _datesRuled;
+            set
+            {
+                _datesRuled = value;
+                // Single date, assume to be 1 year
+                if (!_datesRuled.Contains('-'))
+                {
+                    _yearsInReign = 1;
+                }
+                // The date consists of 2 years
+                else if (!_datesRuled.EndsWith('-'))
+                {
+                    var years = _datesRuled.Split('-');
+                    _yearsInReign = int.Parse(years[1]) - int.Parse(years[0]);
+                }
+                // The date is one year but with a dash, remove the dash and subtract from current Date
+                else
+                {
+                    _yearsInReign = DateTime.Today.Year - int.Parse(_datesRuled.TrimEnd('-'));
+                }
+            }
+        }
+
+        public int YearsInReign => _yearsInReign;
 
         public override string ToString()
         {
-            return $"Name: {nm} of {hse} \nCity: {cty} \nRuled in: {yrs}";
-        }
-
-        // Calculate and return how long given king has ruled for
-        public int GetRuleTime()
-        {
-            // if there is no dask I assume given King has ruled for one year
-            if (!yrs.Contains("-"))
-            {
-                return 1;
-            }
-            else
-            {
-                // If there is only start year and a dash, split throws an exception. I assume those rules are in power until today
-                try
-                {
-                    var years = yrs.Split('-');
-
-                    return int.Parse(years[1]) - int.Parse(years[0]);
-                }
-                catch (FormatException)
-                {
-                    return DateTime.Today.Year - int.Parse(yrs.Replace("-", ""));
-                }
-            }
+            return $"Name: {Name} of {House} \nCity: {City} \nRuled in: {DatesRuled}";
         }
     }
 }
